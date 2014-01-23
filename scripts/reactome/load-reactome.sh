a67277bf4e6f0f7909ad999cce971a0e469c4059
Ken Youens-Clark <kyclark@gmail.com>
2014-01-23 10:25:00 -0700

    adding docs for seach

--- scripts/reactome/load-reactome.sh
+++ scripts/reactome/load-reactome.sh
@@ -1,9 +1,18 @@
+#
+# Set this section...
+#
 DB=reactome_oryza_sativa40
+HOST=cabot
 DATA=/scratch/kyc-tmp/build40/pathways/reactome.tab
 
-mysqladmin -h cabot drop -f $DB
-mysqladmin -h cabot create $DB
-mysql -h cabot $DB < reactome-search-schema.sql
+#
+# Leave the rest alone...
+#
+mysqladmin -h $HOST drop -f $DB
+mysqladmin -h $HOST create $DB
+mysql -h $HOST $DB < reactome-search-schema.sql
 
+#
 # Assumes first line is header, fields declared in schema def order
-mysqlimport -h cabot -c 'search_term,pathway_id,object_id' --local --ignore-lines=1 $DB $DATA
+#
+mysqlimport -h $HOST -c 'search_term,pathway_id,object_id' --local --ignore-lines=1 $DB $DATA
--- scripts/search/delete.sh
+++ scripts/search/delete.sh
@@ -1,3 +1,7 @@
+#
+# Use with caution!
+#
+
 # curl 'http://brie.cshl.edu:8983/solr/grm-search/update?commit=true' -d '<delete><query>*:*</query></delete>'
 
 curl 'http://brie.cshl.edu:8983/solr/grm-search/update?stream.body=<delete><query>module:ensembl_oryza_indica</query></delete>&commit=true'
