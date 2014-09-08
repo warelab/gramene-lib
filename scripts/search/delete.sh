313d68a5310639399237752244fef048cb7b464d
Ken Youens-Clark <kclark@cshl.edu>
2014-09-08 13:04:30 -0400

    new stuff

--- scripts/search/delete.sh
+++ scripts/search/delete.sh
@@ -4,7 +4,7 @@
 
 #curl 'http://brie.cshl.edu:8983/solr/grm-search/update?commit=true' -d '<delete><query>*:*</query></delete>'
 
-curl 'http://brie.cshl.edu:8983/solr/grm-search/update?stream.body=<delete><query>module:pathway*</query></delete>&commit=true'
+curl 'http://brie.cshl.edu:8983/solr/grm-search/update?stream.body=<delete><query>module:reactome*</query></delete>&commit=true'
 
 #curl 'http://brie.cshl.edu:8983/solr/genome_features/update?stream.body=<delete><query>*:*</query></delete>&commit=true'
 
