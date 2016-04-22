8399bfd061c0c9a8b56d47435c937fda92f6b8a0
Andrew Olson <olson@cshl.edu>
2016-04-22 11:32:34 -0400

    updated to allow multi-column matching

--- scripts/utilities/general/paste.pl
+++ scripts/utilities/general/paste.pl
@@ -1,73 +1,110 @@
 #!/usr/bin/env perl
+use strict;
+use warnings;
 
-# paste.pl <file 1> <column> <file 2> <column> <columns to add>
-# finds lines from file 1 that match on the selected column in file 2
-# and appends columsn to add onto file 1
+# paste.pl <file 1> <columns> <file 2> <columns> <columns to add>
+# finds lines from file 1 that match on the selected columns in file 2
+# and appends columns to add onto file 1
+# if no matching row in file2, appends '-'
 # Note: '-' as filename takes input from STDIN
+my $usage = "usage: paste.pl <file 1> <columns> <file 2> <matching columns> <columns to add>\n";
+my $n_args = @ARGV;
+($n_args >= 5) or die $usage;
 
-$n_args = @ARGV;
-($n_args >= 5) or die "usage: paste.pl <file 1> <column> <file 2> <column> <columns to add>";
+my ($fh1,$fh2,@cols1,@cols2,@colsToAdd);
 
-open (FILE1, $ARGV[0]) or die "failed to open '$ARGV[0]'";
-open (FILE2, $ARGV[2]) or die "failed to open '$ARGV[2]'";
+open ($fh1, $ARGV[0]) or die "failed to open '$ARGV[0]'";
 
-$add_all=0;
-if ($ARGV[$n_args-1] eq '+') {
-  $add_all = 1;
-  pop @ARGV;
-  $n_args--;
-  $last_col=-1;
-  $last_col = $ARGV[$n_args-1] if ($n_args>4);
-}
-my $delim;
-while (<FILE2>) {
-  chomp;
-  s/^\s+//;
-  if ($_ =~ /\t/) {
-      $delim = "\t";
-  }
-  elsif ($_ =~ /\s+/) {
-      $delim = "\s+";
-  } else {
-    $delim = "\t";
-  }
-#  @cols = split /\t/, $_;
-  @cols = split /$delim/, $_;
-  $key = $cols[$ARGV[3]];
-  @values=();
-  for($i=4;$i<$n_args;$i++) {
-    $value='-';
-    $value = $cols[$ARGV[$i]] if (defined $cols[$ARGV[$i]]);
-    push @values, $value;
-  }
-  if ($add_all) {
-    for($i=$last_col+1;$i<@cols;$i++) {
-      $value='-';
-      $value = $cols[$i] if (defined $cols[$i]);
-      push @values, $value;
+push @cols1, $ARGV[1];
+for(my $i=2;$i<@ARGV-1;$i++) {
+  if ($fh2) {
+    if (@cols2 < @cols1) {
+      $ARGV[$i] =~ m/^\d+$/ or die "invalid column index 1 '$ARGV[$i]'\n";
+      push @cols2, $ARGV[$i];
+    }
+    else {
+      $ARGV[$i] =~ m/^\d+$/ or die "invalid column index 2 '$ARGV[$i]'\n";
+      push @colsToAdd, $ARGV[$i];
     }
   }
-  $h{$key} = join "\t", @values;
+  elsif (not open($fh2, $ARGV[$i])) {
+    $fh2 =undef;
+    $ARGV[$i] =~ m/^\d+$/ or die "invalid column index 3 '$ARGV[$i]'\n";
+    push @cols1, $ARGV[$i];
+  }
+  else {
+    # we have $fh2 and are done with @cols1
+  }
 }
-close FILE2;
+if ($ARGV[-1] eq '+' or $ARGV[-1] =~ m/^\d+$/) {
+  push @colsToAdd, $ARGV[-1];
+}
+else {
+  die "invalid column index '$ARGV[-1]'\n";
+}
 
-while (<FILE1>) {
+$fh2 and @cols1 and @cols2 and @colsToAdd or die $usage;
+
+my $alsoAdd = 0;
+my $alsoAddFrom = 0;
+if ($colsToAdd[-1] eq '+') {
+  $alsoAdd = 1;
+  pop @colsToAdd;
+  if (@colsToAdd) {
+    $alsoAddFrom = $colsToAdd[-1] + 1;
+  }
+}
+
+my %h;
+my $delim = "\t";
+my $maxColsToAdd = 1;
+while (<$fh2>) {
   chomp;
-  s/^\s+//;
-  if ($_ =~ /\t/) {
-      $delim = "\t";
+  my @cols = split /$delim/, $_;
+  my $key = construct_key(\@cols,\@cols2);
+  my @values;
+  for my $i (@colsToAdd) {
+    if ($i < @cols) {
+      push @values, $cols[$i];
+    }
+    else {
+      push @values, '-';
+    }
   }
-  elsif ($_ =~ /\s+/) {
-      $delim = "\s+";
-  } else {
-    $delim = "\t";
+  if ($alsoAdd) {
+    for (my $i=$alsoAddFrom;$i<@cols;$i++) {
+      push @values, $cols[$i];
+    }
   }
-  @cols = split /$delim/, $_;
-  $key = $cols[$ARGV[1]];
+  if (@values > $maxColsToAdd) {
+    $maxColsToAdd = @values;
+  }
+  $h{$key} = join $delim, @values;
+}
+close $fh2;
+
+while (<$fh1>) {
+  chomp;
+  my @cols = split /$delim/, $_;
+  my $key = construct_key(\@cols,\@cols1);
   if (defined $h{$key}) {
     print "$_\t$h{$key}\n";
   } else {
-    print "$_\t-\n";
+    print join("\t", $_, ('-') x $maxColsToAdd),"\n";
   }
 }
-close FILE1;
+close $fh1;
+
+sub construct_key {
+  my ($aref,$qcols) = @_;
+  my @q;
+  for my $i (@$qcols) {
+    if ($i < @$aref) {
+      push @q, $$aref[$i];
+    }
+    else {
+      push @q, '-';
+    }
+  }
+  return join $delim, @q;
+}
